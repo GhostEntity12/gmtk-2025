3b41d1deb15dd877102c9a53dd72338b7da4d3e0
GhostEntity12 <48895274+GhostEntity12@users.noreply.github.com>
2025-08-03 18:19:28 +1000

    Working on character damage

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -1,22 +1,41 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Character : MonoBehaviour
 {
-	[SerializeField] private int health = 5;
+	[SerializeField] private int maxHealth = 5;
+	[SerializeField] private int health;
 
 	[SerializeField] private List<Die> dice;
-	
+
+	[SerializeField] Image characterImage;
 	[SerializeField] private Sprite spriteDefault;
 	[SerializeField] private Sprite spriteDamage;
+	[SerializeField] AnimationCurve hurtCurve;
+
+	private void Start()
+	{
+		health = maxHealth;
+	}
+
+	[ContextMenu("Test Damage")]
+	void td() => TakeDamage(0);
 
 	public void TakeDamage(int damage)
 	{
 		health -= damage;
-		
-		if (health < 0)
+
+		if ((float)health/maxHealth <= 0.5f)
+		{
+			// character is badly damaged
+			characterImage.sprite = spriteDamage;
+		}
+
+		else if (health <= 0)
 		{
 			// character is dead
 		}
+		LeanTween.color(characterImage.rectTransform, Color.red, 0.5f).setEase(hurtCurve);
 	}
 }
